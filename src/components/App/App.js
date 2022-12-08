fcf87978b236689028b7bea66d47b88f1ba9221c
Joshua Comeau <joshwcomeau@gmail.com>
2022-12-08 12:37:16 -0500

    Exercise 4 solution

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
+import ToastProvider from '../ToastProvider';
 import ToastPlayground from '../ToastPlayground';
 import Footer from '../Footer';
 
 function App() {
   return (
-    <>
+    <ToastProvider>
       <ToastPlayground />
       <Footer />
-    </>
+    </ToastProvider>
   );
 }
 
