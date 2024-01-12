25688f513ff34a55ae950f2f6c642524f95c8c6b
Kimia Abtahi <kimia.abtahi@bibsent.no>
2024-01-12 17:00:38 +0100

    render and live edit Toast component

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 import {
   AlertOctagon,
   AlertTriangle,
   CheckCircle,
   Info,
   X,
-} from 'react-feather';
+} from "react-feather";
 
-import VisuallyHidden from '../VisuallyHidden';
+import VisuallyHidden from "../VisuallyHidden";
 
-import styles from './Toast.module.css';
+import styles from "./Toast.module.css";
 
 const ICONS_BY_VARIANT = {
   notice: Info,
@@ -18,21 +18,35 @@ const ICONS_BY_VARIANT = {
   error: AlertOctagon,
 };
 
-function Toast() {
+function Toast({ variant, children, setShowToast }) {
   return (
-    <div className={`${styles.toast} ${styles.notice}`}>
-      <div className={styles.iconContainer}>
-        <Info size={24} />
-      </div>
-      <p className={styles.content}>
-        16 photos have been uploaded
-      </p>
-      <button className={styles.closeButton}>
+    <div className={`${styles.toast} ${styles[variant]}`}>
+      <div className={styles.iconContainer}>{getIcon(variant)}</div>
+      <p className={styles.content}>{children}</p>
+      <button
+        onClick={() => setShowToast(false)}
+        className={styles.closeButton}
+      >
         <X size={24} />
         <VisuallyHidden>Dismiss message</VisuallyHidden>
       </button>
     </div>
   );
 }
+
+const getIcon = (variant) => {
+  switch (variant) {
+    case "notice":
+      return <Info />;
+    case "warning":
+      return <AlertTriangle />;
+    case "success":
+      return <CheckCircle />;
+    case "error":
+      return <AlertOctagon />;
+    default:
+      return <Info />;
+  }
+};
 
 export default Toast;
--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -3,6 +3,7 @@ import React from "react";
 import Button from "../Button";
 
 import RadioButton from "../RadioButton/RadioButton";
+import Toast from "../Toast/Toast";
 import styles from "./ToastPlayground.module.css";
 
 const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
@@ -14,7 +15,7 @@ function ToastPlayground() {
     VARIANT_OPTIONS[0]
   );
 
-  console.log(selectedVariant);
+  const [showToast, setShowToast] = React.useState(false);
 
   return (
     <div className={styles.wrapper}>
@@ -23,6 +24,11 @@ function ToastPlayground() {
         <h1>Toast Playground</h1>
       </header>
 
+      {showToast && (
+        <Toast variant={selectedVariant} setShowToast={setShowToast}>
+          {message}
+        </Toast>
+      )}
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
           <label
@@ -59,7 +65,7 @@ function ToastPlayground() {
         <div className={styles.row}>
           <div className={styles.label} />
           <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-            <Button>Pop Toast!</Button>
+            <Button onClick={() => setShowToast(true)}>Pop Toast!</Button>
           </div>
         </div>
       </div>
